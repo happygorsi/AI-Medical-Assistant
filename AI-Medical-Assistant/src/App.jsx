492aa2bedfd26a9d23385d85d1a8ebb55226f65f
learning-git <gorsihappy152@gmail.com>
2026-04-09 22:56:08 +0530

    add some basic logos

--- AI-Medical-Assistant/src/App.jsx
+++ AI-Medical-Assistant/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { FaArrowUp, FaRobot } from "react-icons/fa";
 
 const App = () => {
   return (
     <div className="h-screen bg-blue-200 flex flex-col items-center bg-white-100">
       {/* Header */}
-      <div className=' bg-gray-900 text-white w-full p-6 text-center font-semibold text-xl rounded-b-4xl shadow-lg '>
-        <h2>AI Medical Assistant</h2>
+      <div className=' flex justify-center items-center gap-2 bg-gray-900 text-white w-full p-6 text-center font-semibold text-xl rounded-b-4xl shadow-lg '>
+        <h2>AI Medical Assistant </h2>
+        <FaRobot />
       </div>
       {/* Main Content */}
       <div className=' bg-blue-100 w-3/4 flex-1 p-8 text-left border-2 border-gray-300 overflow-y-auto rounded-lg shadow-md mb-4 mt-3'>
@@ -18,8 +20,8 @@ const App = () => {
           placeholder="Type Your Symptoms Here..."
           className=" bg-white w-2/4 p-3 text-gray-800 placeholder:text-gray-500 border rounded-full shadow-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-        <button className="bg-blue-500 text-white p-3 rounded-full shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 ml-2">
-          Submit
+        <button className="bg-blue-500 text-white p-4.5 rounded-full shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 ml-2">
+          <FaArrowUp className="text-xl hover:scale-110 transition" />
         </button>
       </div>
 
